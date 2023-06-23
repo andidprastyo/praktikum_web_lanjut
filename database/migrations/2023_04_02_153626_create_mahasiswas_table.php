25f2beef8fed7b11b304c4db9db9eab44c6ceae9
= <=>
2023-06-24 03:03:09 +0700

    Praktikum 7

---
+++ database/migrations/2023_04_02_153626_create_mahasiswas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nim', 16)->unique();
+            $table->string('Nama', 64);
+            $table->string('Kelas', 16);
+            $table->string('Jurusan', 32);
+            $table->string('No_Handphone', 16);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+};
